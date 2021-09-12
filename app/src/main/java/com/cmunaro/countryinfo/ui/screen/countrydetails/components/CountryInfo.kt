6180f3915def58057182a255486d377e8b71be92
cristiano munaro <munaro07@gmail.com>
2021-09-12 21:14:10 +0200

    Extract some strings and fix column size.

--- app/src/main/java/com/cmunaro/countryinfo/ui/screen/countrydetails/components/CountryInfo.kt
+++ app/src/main/java/com/cmunaro/countryinfo/ui/screen/countrydetails/components/CountryInfo.kt
@@ -1,28 +1,36 @@
 package com.cmunaro.countryinfo.ui.screen.countrydetails.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.cmunaro.countryinfo.R
 import com.cmunaro.countryinfo.ui.screen.countrydetails.CountryDefinition
 
 @Composable
 fun CountryInfo(country: CountryDefinition) {
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
         Text(
             text = "${country.name} (${country.countryCode})",
             style = MaterialTheme.typography.h4,
             modifier = Modifier.padding(4.dp)
         )
-        CountryExpandableInfo(title = "Continent", country.continent)
-        CountryExpandableInfo(title = "Capital", country.capital)
-        CountryExpandableInfo(title = "Currency", country.currency)
-        CountryExpandableInfo(title = "Language", *country.languages.toTypedArray())
-        CountryExpandableInfo(title = "Phone", country.phone)
+        CountryExpandableInfo(title = stringResource(R.string.continent), country.continent)
+        CountryExpandableInfo(title = stringResource(R.string.capital), country.capital)
+        CountryExpandableInfo(title = stringResource(R.string.currency), country.currency)
+        CountryExpandableInfo(
+            title = stringResource(R.string.language),
+            *country.languages.toTypedArray()
+        )
+        CountryExpandableInfo(title = stringResource(R.string.phone), country.phone)
     }
 }
 
