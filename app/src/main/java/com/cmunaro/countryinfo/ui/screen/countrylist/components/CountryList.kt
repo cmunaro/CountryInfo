83212b5bbb97ab90e5b328cf19ba46a0cb1ba72b
cristiano munaro <munaro07@gmail.com>
2021-09-12 18:42:20 +0200

    Enhance graphic.

--- app/src/main/java/com/cmunaro/countryinfo/ui/screen/countrylist/components/CountryList.kt
+++ app/src/main/java/com/cmunaro/countryinfo/ui/screen/countrylist/components/CountryList.kt
@@ -1,5 +1,6 @@
 package com.cmunaro.countryinfo.ui.screen.countrylist.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -18,7 +19,8 @@ fun CountryList(
     items: List<CountryListEntry>
 ) {
     LazyColumn(
-        contentPadding = PaddingValues(8.dp)
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         items(
             items,
--- app/src/main/java/com/cmunaro/countryinfo/ui/screen/countrylist/components/CountryRow.kt
+++ app/src/main/java/com/cmunaro/countryinfo/ui/screen/countrylist/components/CountryRow.kt
@@ -5,13 +5,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cmunaro.countryinfo.ui.screen.countrylist.CountryListEntry
 import com.cmunaro.countryinfo.ui.shared.CountryFlag
 import com.cmunaro.countryinfo.ui.theme.CountryInfoTheme
@@ -21,7 +21,7 @@ fun CountryRow(
     country: CountryListEntry,
     onClick: () -> Unit
 ) {
-    val fontSize = MaterialTheme.typography.h4.fontSize
+    val fontSize = 30.sp
     Row(
         modifier = Modifier
             .fillMaxWidth()
