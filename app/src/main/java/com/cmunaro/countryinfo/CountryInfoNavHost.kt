0cb7b890acbdc7c36f80b328791fed1cad12f43b
cristiano munaro <munaro07@gmail.com>
2021-09-12 19:22:47 +0200

    Render preview of CountryDetailsScreen.

--- app/src/main/java/com/cmunaro/countryinfo/CountryInfoNavHost.kt
+++ app/src/main/java/com/cmunaro/countryinfo/CountryInfoNavHost.kt
@@ -1,12 +1,15 @@
 package com.cmunaro.countryinfo
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.cmunaro.countryinfo.ui.screen.countrydetails.CountryDetailsScreen
+import com.cmunaro.countryinfo.ui.screen.countrydetails.CountryDetailsViewModel
 import com.cmunaro.countryinfo.ui.screen.countrylist.CountryListScreen
+import org.koin.androidx.compose.getViewModel
 
 @Composable
 fun CountryInfoNavHost(navController: NavHostController) = NavHost(
@@ -22,7 +25,9 @@ fun CountryInfoNavHost(navController: NavHostController) = NavHost(
                 ?.getString("countryCode")
                 ?: return@composable
         }
-        CountryDetailsScreen(countryCode)
+        val viewModel: CountryDetailsViewModel = getViewModel()
+        LaunchedEffect(countryCode) { viewModel.getInfoOf(countryCode) }
+        CountryDetailsScreen(viewModelStateFlow = viewModel.state)
     }
 }
 
--- app/src/main/java/com/cmunaro/countryinfo/ui/screen/countrydetails/CountryDetailsScreen.kt
+++ app/src/main/java/com/cmunaro/countryinfo/ui/screen/countrydetails/CountryDetailsScreen.kt
@@ -1,22 +1,46 @@
 package com.cmunaro.countryinfo.ui.screen.countrydetails
 
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
 import com.cmunaro.countryinfo.ui.screen.countrydetails.components.CountryInfo
 import com.cmunaro.countryinfo.ui.shared.Loading
-import org.koin.androidx.compose.getViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 @Composable
 fun CountryDetailsScreen(
-    countryCode: String,
-    viewModel: CountryDetailsViewModel = getViewModel()
+    viewModelStateFlow: StateFlow<CountryDetailsState>
 ) {
-    LaunchedEffect(countryCode) { viewModel.getInfoOf(countryCode) }
-    val state by viewModel.state.collectAsState()
+    val state by viewModelStateFlow.collectAsState()
     state.country?.let { country ->
         CountryInfo(country = country)
     }
     Loading(isVisible = state.isLoading)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CountryDetailsScreenPreview() {
+    MaterialTheme {
+        CountryDetailsScreen(
+            viewModelStateFlow = MutableStateFlow(
+                CountryDetailsState(
+                    isLoading = false,
+                    error = false,
+                    CountryDefinition(
+                        "IT",
+                        "Italy",
+                        "Rome",
+                        "Europe",
+                        "Euro",
+                        listOf("Italian"),
+                        "39"
+                    )
+                )
+            )
+        )
+    }
 }
